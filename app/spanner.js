d8c43714512be12ad4a419b330d9c82634adfd3f
y-ohgi <ohgi.211@gmail.com>
2018-03-05 04:28:53 +0900

    impl selectとinsertまで

---
+++ app/spanner.js
@@ -0,0 +1,89 @@
+const Spanner = require('@google-cloud/spanner')
+
+const ENV = process.env
+
+const database = () => {
+  const spanner = Spanner({
+    projectId: ENV.PROJECT_ID
+  })
+  
+  const instance = spanner.instance(ENV.INSTANCE_ID)
+  return instance.database(ENV.DATABASE_ID)
+}
+
+const tables = table => {
+  return database().table(table)
+}
+
+//FIXME: idは生成方法変更を加味してここで挿入したいが、インターリーブが微妙
+const insert = (table, values) => new Promise(
+  (resolve, reject) => {
+    const vals = values.map(elm => {
+      elm['created_at'] = new Date()
+      return elm
+    })
+
+    tables(table)
+      .insert(vals)
+      .then(() => resolve(vals))
+      .catch(err => {
+        console.log(err)
+        reject(err)
+      })
+      .then(err => database().close())
+  }
+)
+
+const select = (table, columns) => new Promise(
+  (resolve, reject) => {
+    const query = {
+      keySet: {
+        all: true
+      },
+      columns: columns
+    }
+
+    tables(table)
+      .read(query)
+      .then(res => resolve(res))
+      .catch(err => {
+        console.log(err)
+        reject(err)
+      })
+      .then(err => database().close())
+  }
+)
+
+//TODO: インデックスは明示的に指定しなければならないっぽいので、する。
+//  => https://cloud.google.com/spanner/docs/getting-started/nodejs/#read_using_the_index
+//FIXME: target_idがダサめ
+const find = (table, columns, id, target_id = null) => new Promise(
+  (resolve, reject) => {
+    const target = target_id || columns.filter(col => col.match('_id$'))
+    const cols = columns.join(',')
+    
+    const query = {
+      sql: `SELECT ${cols} FROM ${table} WHERE ${target} = @id`,
+      params: {
+        id: id
+      }
+    }
+
+    console.log(query)
+
+    database()
+      .run(query)
+      .then(res => resolve(res[0]))
+      .catch(err => {
+        console.log(err)
+        reject(err)
+      })
+      .then(err => database().close())
+  }
+)
+  
+module.exports = {
+ insert,
+  select,
+  find
+}
--- index.js
+++ index.js
@@ -1,17 +1,121 @@
 const express = require('express')
+const uuid = require('uuid/v4')
+const bodyParser = require('body-parser');
+const spanner = require('./app/spanner.js')
+
+const ENV = process.env
 const app = express()
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  console.log(req)
+  res.send('hoge')
 })
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/blogs', (req, res) => {
+  spanner.
+    select('blogs', ['blog_id', 'title', 'body', 'created_at'])
+    .then(data => {res.json(data)})
+    .catch(err => {
+      console.log(err)
+      res.status(500).send(err)
+      return 500
+    })
+    .then(err => console.log(`[${req.method}] ${err ? err : 200} ${req.url}`) )
+})
+
+app.post('/blogs', (req, res) => {
+  const values = [
+    {
+      blog_id: uuid(),
+      title: req.body.title || '',
+      body: req.body.body || ''
+    }
+  ]
+  
+  spanner.insert('blogs', values)
+    .then(data => {res.json(data)})
+    .catch(err => {
+      res.status(500).send(err)
+      return 500
+    })
+    .then(err => console.log(`[${req.method}] ${err ? err : 200} ${req.url}`) )
+})
+
+app.get('/blogs/:id', (req, res) => {
+  Promise.all([
+    spanner.find('blogs', ['blog_id', 'title', 'body', 'created_at'], req.params.id),
+    spanner.find('comments', ['blog_id', 'comment_id', 'name', 'body', 'created_at'], req.params.id, 'blog_id')
+  ])
+    .then(datas => {
+      res.json({
+        blog: datas[0][0],
+        comments: datas[1]
+      })
+    })
+    .catch(err => {
+      res.status(500).send(err)
+      return 500
+    })
+    .then(err => console.log(`[${req.method}] ${err ? err : 200} ${req.url}`) )
+})
+
+//TOOD: Read/Writeトラン
+app.post('/blogs/:id/comments', (req, res) => {
+  const values = [
+    {
+      blog_id: req.params.id,
+      comment_id: uuid(),
+      name: req.body.name || '名無し',
+      body: req.body.body || ''
+    }
+  ]
+  
+  spanner.insert('comments', values)
+    .then(data => {res.json(data)})
+    .catch(err => {
+      res.status(500).send(err)
+      return 500
+    })
+    .then(err => console.log(`[${req.method}] ${err ? err : 200} ${req.url}`) )
 })
 
 const server = app.listen(8080, () => {
   const host = server.address().address
   const port = server.address().port
 
-  console.log(`Example app listening at http://${host}:${port}`);
+  console.log(`Example app listening at http://${host}:${port}`)
 })
+
+
+
+// const spanner = Spanner({
+//   projectId: ENV.PROJECT_ID
+// })
+
+// const instance = spanner.instance(ENV.INSTANCE_ID);
+// const database = instance.database(ENV.DATABASE_ID);
+
+// const blogs = database.table('blogs');
+
+// blogs
+//   .insert([
+//     {
+//       blog_id: uuid(),
+//       title: "test",
+//       body: "test body",
+//       created_at: new Date()
+//     }
+//   ])
+//   .then(data => {
+//     console.log(data)
+//   })
+//   .catch(err => {
+//     console.log(err)
+//   })
+//   .then(() => {
+//     console.log('closing connection')
+//     database.close()
+
+//     res.send('Hello World!')
+//   })
